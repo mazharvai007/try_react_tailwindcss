36e42f736d66678c78bb84947ff06d2f346dea03
Mazharul <mazharul@Mazharuls-MacBook-Pro.local>
2021-01-20 00:27:42 +0600

    exploring about state hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import CounterExample from './Components/CounterExample/CounterExample';
 import HelloWorld from './Components/HelloWorld/HelloWorld';
 
 function App() {
 	return (
 		<div className='App'>
 			<div>
-				<HelloWorld name='Jane' />
+				{/* <HelloWorld name='Jane' /> */}
+				<CounterExample />
 			</div>
 		</div>
 	);
