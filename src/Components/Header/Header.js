85d31b21dd03734025af44aa0dff2454fa126a2d
Mazharul <mazharul@Mazharuls-MacBook-Pro.local>
2021-01-20 02:19:28 +0600

    Made header navigation and footer component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+
+function Header() {
+	return (
+		<header className='border-b p-3 flex justify-between items-center'>
+			<span className='font-bold'>AppName</span>
+			<Navigation />
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+function Navigation() {
+	const [showManu, setShowMenu] = useState(false);
+
+	let menu;
+	let menuMask;
+
+	if (showManu) {
+		menu = (
+			<div className='fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow'>
+				The Menu
+			</div>
+		);
+		menuMask = (
+			<div
+				className='bg-black-t-50 fixed top-0 left-0 w-full h-full z-50'
+				onClick={() => setShowMenu(false)}></div>
+		);
+	}
+
+	return (
+		<nav>
+			<span className='text-xl'>
+				<FontAwesomeIcon
+					icon={faBars}
+					onClick={() => setShowMenu(!showManu)}
+				/>
+			</span>
+
+			{menuMask}
+			{menu}
+		</nav>
+	);
+}
+
+export default Navigation;
