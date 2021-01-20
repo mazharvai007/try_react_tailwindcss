e6fde698e11d61cd90c69343ad30b3dc9b786a9f
Mazharul <mazharul@Mazharuls-MacBook-Pro.local>
2021-01-20 22:27:32 +0600

    added animation in the navigation

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,25 +1,20 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useTransition, animated } from 'react-spring';
 
 function Navigation() {
 	const [showManu, setShowMenu] = useState(false);
-
-	let menu;
-	let menuMask;
-
-	if (showManu) {
-		menu = (
-			<div className='fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow'>
-				The Menu
-			</div>
-		);
-		menuMask = (
-			<div
-				className='bg-black-t-50 fixed top-0 left-0 w-full h-full z-50'
-				onClick={() => setShowMenu(false)}></div>
-		);
-	}
+	const maskTransition = useTransition(showManu, null, {
+		from: { position: 'absolute', opacity: 0 },
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },
+	});
+	const menuTransition = useTransition(showManu, null, {
+		from: { opacity: 0, transform: 'translateX(-100%)' },
+		enter: { opacity: 1, transform: 'translateX(0%)' },
+		leave: { opacity: 0, transform: 'translateX(-100%)' },
+	});
 
 	return (
 		<nav>
@@ -30,8 +25,33 @@ function Navigation() {
 				/>
 			</span>
 
-			{menuMask}
-			{menu}
+			{maskTransition.map(
+				({ item, key, props }) =>
+					item && (
+						<animated.div
+							key={key}
+							style={props}
+							className='bg-black-t-50 fixed top-0 left-0 w-full h-full z-50'
+							onClick={() => setShowMenu(false)}></animated.div>
+					)
+			)}
+
+			{menuTransition.map(
+				({ item, key, props }) =>
+					item && (
+						<animated.div
+							key={key}
+							style={props}
+							className='fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow p-3'>
+							<span>The Menu</span>
+							<ul>
+								<li>
+									<a href='#'>Home</a>
+								</li>
+							</ul>
+						</animated.div>
+					)
+			)}
 		</nav>
 	);
 }
