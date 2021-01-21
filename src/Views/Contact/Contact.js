dbc252e956108c5c6235c9899523531f6167fe3b
Mazharul <mazharul@Mazharuls-MacBook-Pro.local>
2021-01-21 06:11:11 +0600

    Managing routing using react router and made views

---
+++ src/Views/Contact/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+	return (
+		<div>
+			<h1 className='font-bold text-2xl'>This is contact Page.</h1>
+		</div>
+	);
+}
+
+export default Contact;
---
+++ src/Views/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+	return (
+		<div>
+			<h1 className='font-bold text-2xl'>This is homepage.</h1>
+		</div>
+	);
+}
+
+export default Home;
